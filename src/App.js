a938b8c05715eff457560316c5527ad8f3c7546e
Damien Minter <Damo@MacBook-Pro-2.local>
2020-10-24 19:28:55 +1100

    Clean up Boiler Plate Code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import Main from "./Main/Main";
 import Navbar from "./Navigation/Navbar";
 import Routes from "./Navigation/Routes";
 
