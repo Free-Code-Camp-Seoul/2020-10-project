3174180260bcade6bb4f75bfcb0dfeda9e2de8bd
Damien Minter <Damo@MacBook-Pro-2.local>
2020-11-01 19:38:57 +1100

    basic form info - get image - text input

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -1,5 +1,11 @@
 import React from "react";
+import AddProduct from "./AddProduct";
 
 export default function Admin() {
-  return <div>Here is the Admin page</div>;
+  return (
+    <div>
+      <h2>Here is the Admin page</h2>
+      <AddProduct />
+    </div>
+  );
 }
