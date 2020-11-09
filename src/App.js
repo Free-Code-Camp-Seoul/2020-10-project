1657f5e9c181d24bc75cc917079d934adc76c7b4
Damien Minter <Damo@MacBook-Pro-2.local>
2020-11-09 14:04:46 +1100

    Clear linting errors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import Navbar from "./Navigation/Navbar";
 import Routes from "./Navigation/Routes";
@@ -7,17 +7,16 @@ import CartContext from "./Cart/models/CartContext";
 import React from "react";
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <CartContext.Provider>
-                    <Navbar/>
-                    <Routes/>
-                </CartContext.Provider>
-
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <CartContext.Provider>
+          <Navbar />
+          <Routes />
+        </CartContext.Provider>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
